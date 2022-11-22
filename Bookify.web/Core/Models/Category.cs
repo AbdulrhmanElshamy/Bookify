47076ee9932ebdfabc1daabfc10c71670127d8e1
AbDulrhman Elshamy <AbdulrhmanElshamy12@gmail.com>
2022-11-22 23:14:35 +0200

    update connectionstring Add categories table

---
+++ Bookify.web/Core/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookify.web.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? LastUpdatedDate { get; set; }
+
+    }
+}
